7747c8e2433139bf6b1f9c8330bbf26b89e5c338
Sander <7okay@mail.ru>
2021-09-14 22:38:10 +0500

    создание навигации, страницы создания задания

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,61 @@
 <template>
   <div id="app">
-    <router-view/>
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+    >
+      <router-link 
+      v-for="link of links"
+      :key="link.index"
+      tag="el-menu-item" :index="link.index" :to="{ name: link.routeName }"
+      active-class="is-active"
+      exact
+        >{{link.title}}</router-link
+      >
+    </el-menu>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
 
+@Component({})
+export default class App extends Vue {
+  activeIndex = '1';
+  links = [
+    {
+      index: '1',
+      title: "Список задач",
+      routeName: "Home"
+    },
+
+    {
+      index: '2',
+      title: "Создать новую задачу",
+      routeName: "CreateTask"
+    }
+  ]
+}
+</script>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+}
+
+.is-active {
+color: rgb(255, 208, 75) !important;
+    border-bottom-color: rgb(255, 208, 75);
+    background-color: rgb(84, 92, 100);
+    }
+
+    .page-title {
+    margin-bottom: 1.5rem;
+    }
 </style>
---
+++ src/views/CreateTask.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="create-task">
+    <v-container>
+      <h1 class="page-title">Создать задачу</h1>
+
+      <el-form ref="form" :model="form" label-width="120px">
+        <el-row>
+          <el-col :span="14">
+            <!-- заголовок -->
+            <el-form-item label="Заголовок задачи">
+              <el-input v-model="form.title"></el-input>
+            </el-form-item>
+            <!-- описание -->
+            <el-form-item label="Описание задачи">
+              <el-input
+                v-model="form.description"
+                type="textarea"
+                :rows="2"
+                placeholder="Кратко опишите задачу"
+              ></el-input>
+              </el-form-item>
+
+              <!-- button -->
+              <el-form-item>
+                <el-button type="success" @click="createTast">Создать</el-button>
+              
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+    </v-container>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import VContainer from "@/components/v-container.vue";
+import Task from "@/types/Task";
+
+@Component({
+  name: "CreateTask",
+  components: {
+    VContainer,
+  },
+})
+export default class CreateTask extends Vue {
+  form = {} as Task;
+
+  createTast():void {
+      const newTask: Task = {
+          id: Date.now().toString(32),
+          title: this.form.title,
+          description: this.form.description,
+          status: 'created'
+      }
+    console.log('newTask',  newTask)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
